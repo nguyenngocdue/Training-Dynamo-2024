9d0cec424a46c6664b4505c238834d98f349d8c1
nguyenngocdue <nguyenngocdue>
2024-06-18 21:09:56 +0700

    translatePointByField

--- DesignScript.py
+++ DesignScript.py
@@ -9,29 +9,12 @@ from System.Collections.Generic import *
 
 clr.AddReference("ProtoGeometry")
 from Autodesk.DesignScript.Geometry import *
-from Points import getMaxPoints
-
+from Points import *
 
 #Preparing input from dynamo to revit
 elements = UnwrapElement(IN[0])
 field = IN[1]
 distance = IN[2]
 
-def translatePointByField(points, field = 'x', distance = 0):
-    newPoints = []
-    for point in points:
-        x = point.X
-        y = point.Y
-        z = point.Z
-        
-        if field == 'x' or field == 'X':
-            x = x + distance
-        if field == 'y' or field == 'Y':
-            y = y + distance
-        if field == 'z' or field == 'Z':
-            z = z + distance
-        _point = Point.ByCoordinates(x,y,z)
-        newPoints.append(_point)
-    return newPoints
 
 OUT = translatePointByField(elements, field, distance)
--- Library/Points.py
+++ Library/Points.py
@@ -1,3 +1,10 @@
+import clr 
+import sys
+from System.Collections.Generic import *
+
+clr.AddReference("ProtoGeometry")
+from Autodesk.DesignScript.Geometry import *
+
 def getMaxPoints(points):
     maxPointZ = points[0].Z
     tempPoint = points[0]
@@ -6,3 +13,19 @@ def getMaxPoints(points):
             maxPointZ = point.Z
             tempPoint = point
     return tempPoint
+def translatePointByField(points, field = 'x', distance = 0):
+    newPoints = []
+    for point in points:
+        x = point.X
+        y = point.Y
+        z = point.Z
+        
+        if field == 'x' or field == 'X':
+            x = x + distance
+        if field == 'y' or field == 'Y':
+            y = y + distance
+        if field == 'z' or field == 'Z':
+            z = z + distance
+        _point = Point.ByCoordinates(x,y,z)
+        newPoints.append(_point)
+    return newPoints
