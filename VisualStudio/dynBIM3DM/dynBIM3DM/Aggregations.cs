00962c060a9a6f4ddbaac12be5e15b84a0155740
nguyenngocdue <nguyenngocdue>
2024-08-13 20:14:53 +0700

    minor

---
+++ VisualStudio/dynBIM3DM/dynBIM3DM/Aggregations.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.DesignScript.Runtime;
+
+namespace dynBIM3DM.Math
+{
+    //[IsVisibleInDynamoLibrary(false)]
+    public static class Operations
+    {
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+    }
+}
+
+namespace MyCustomNodes.Utils
+{
+    public static class Logger
+    {
+        public static void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
